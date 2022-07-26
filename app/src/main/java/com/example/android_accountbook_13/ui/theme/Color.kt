a4554d74f5c8ea275ca362adbdd12ac0ea540d44
KwangHyun Choi <camp_kwangddang12@woowafriends.com>
2022-07-26 10:51:16 +0900

    [ADD] #2 Color 등록

--- app/src/main/java/com/example/android_accountbook_13/ui/theme/Color.kt
+++ app/src/main/java/com/example/android_accountbook_13/ui/theme/Color.kt
@@ -6,3 +6,54 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+/**
+ *  Primary Color
+ */
+val OffWhite = Color(0xFFF7F6F3)
+val White = Color(0xFFFFFFFF)
+val White50 = Color(0x80FFFFFF)
+val Yellow = Color(0xFFF5B853)
+val Purple = Color(0xFF524D90)
+val LightPurple = Color(0xFFA79FCB)
+val Purple40 = Color(0x66A79FCB)
+val Red = Color(0xFFE75B3F)
+
+/**
+ *  Secondary Color
+ */
+val Blue1 = Color(0xFF4A6CC3)
+val Blue2 = Color(0xFF2E86C7)
+val Blue3 = Color(0xFF4CA1DE)
+val Blue4 = Color(0xFF48C2E9)
+val Blue5 = Color(0xFF6ED5EB)
+
+val Green1 = Color(0xFF9FE7C8)
+val Green2 = Color(0xFF94D3CC)
+val Green3 = Color(0xFF4CB8B8)
+val Green4 = Color(0xFF40B98D)
+val Green5 = Color(0xFF2FA488)
+
+val Purple1 = Color(0xFF625EBA)
+val Purple2 = Color(0xFF817DCE)
+val Purple3 = Color(0xFF9B7DCE)
+val Purple4 = Color(0xFFB391EB)
+val Purple5 = Color(0xFFD092E2)
+
+val Pink1 = Color(0xFFF1B4EF)
+val Pink2 = Color(0xFFF4AEE1)
+val Pink3 = Color(0xFFF396B8)
+val Pink4 = Color(0xFFDC5D7B)
+val Pink5 = Color(0xFFCB588F)
+
+val Olive1 = Color(0xFF9BD182)
+val Olive2 = Color(0xFFA3CB7A)
+val Olive3 = Color(0xFFB5CC7A)
+val Olive4 = Color(0xFFCCD67A)
+val Olive5 = Color(0xFFEAE07C)
+
+val Yellow1 = Color(0xFFEDCF65)
+val Yellow2 = Color(0xFFEBC374)
+val Yellow3 = Color(0xFFE1AD60)
+val Yellow4 = Color(0xFFE29C4D)
+val Yellow5 = Color(0xFFE39145)
