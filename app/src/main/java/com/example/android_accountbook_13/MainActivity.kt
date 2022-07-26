e302f4f00bd7a75a8b55346fb1ca93331afaca11
KwangHyun Choi <camp_kwangddang12@woowafriends.com>
2022-07-26 10:13:07 +0900

    [CHORE] #1 프로젝트 세팅

---
+++ app/src/main/java/com/example/android_accountbook_13/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.android_accountbook_13
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.android_accountbook_13.ui.theme.Androidaccountbook13Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Androidaccountbook13Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Androidaccountbook13Theme {
+        Greeting("Android")
+    }
+}
