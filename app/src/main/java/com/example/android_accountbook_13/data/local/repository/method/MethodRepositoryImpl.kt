a3480d0b6974c7cb1981fceee6587ae35140b775
KwangHyun Choi <camp_kwangddang12@woowafriends.com>
2022-08-03 14:03:46 +0900

    [ADD] #40 각 repository에 error message 추가

--- app/src/main/java/com/example/android_accountbook_13/data/local/repository/method/MethodRepositoryImpl.kt
+++ app/src/main/java/com/example/android_accountbook_13/data/local/repository/method/MethodRepositoryImpl.kt
@@ -1,12 +1,8 @@
 package com.example.android_accountbook_13.data.local.repository.method
 
-import android.database.Cursor
 import com.example.android_accountbook_13.data.DataResponse
-import com.example.android_accountbook_13.data.dto.History
 import com.example.android_accountbook_13.data.dto.Method
 import com.example.android_accountbook_13.data.local.datasource.LocalDataSourceImpl
-import com.example.android_accountbook_13.utils.getCategoryFromCursor
-import com.example.android_accountbook_13.utils.getHistoryFromCursor
 import com.example.android_accountbook_13.utils.getMethodFromCursor
 import javax.inject.Inject
 
@@ -14,7 +10,7 @@ class MethodRepositoryImpl @Inject constructor(
     private val localDataSource: LocalDataSourceImpl
 ) : MethodRepository {
     override fun getAllMethod(): DataResponse<List<Method>> {
-        val cursor = localDataSource.getAllMethod().getOrNull() ?: return DataResponse.Error()
+        val cursor = localDataSource.getAllMethod().getOrNull() ?: return DataResponse.Error("결제 수단을 불러오지 못 했습니다.")
         val itemList = mutableListOf<Method>()
         while (cursor.moveToNext()) {
             itemList.add(getMethodFromCursor(cursor,0))
@@ -23,26 +19,23 @@ class MethodRepositoryImpl @Inject constructor(
     }
 
     override fun getMethod(id: Int): DataResponse<Method> {
-        val cursor = localDataSource.getMethod(id).getOrNull() ?: return DataResponse.Error()
+        val cursor = localDataSource.getMethod(id).getOrNull() ?: return DataResponse.Error("결제 수단을 불러오지 못 했습니다.")
         cursor.moveToNext()
         return DataResponse.Success(getMethodFromCursor(cursor,0))
     }
 
     override fun insertMethod(method : Method): DataResponse<Unit> {
-        if(localDataSource.insertMethod(method).getOrNull() == null)
-            return DataResponse.Error()
+        if(localDataSource.insertMethod(method).getOrNull() == null) return DataResponse.Error("결제 수단을 추가하지 못 했습니다.")
         return DataResponse.Success(Unit)
     }
 
     override fun updateMethod(method : Method): DataResponse<Unit> {
-        if(localDataSource.updateMethod(method).getOrNull() == null)
-            return DataResponse.Error()
+        if(localDataSource.updateMethod(method).getOrNull() == null) return DataResponse.Error("결제 수단을 수정하지 못 했습니다.")
         return DataResponse.Success(Unit)
     }
 
     override fun deleteMethod(methodId: Int): DataResponse<Unit> {
-        if(localDataSource.deleteMethod(methodId).getOrNull() == null)
-            return DataResponse.Error()
+        if(localDataSource.deleteMethod(methodId).getOrNull() == null) return DataResponse.Error("결제 수단을 삭제하지 못 했습니다.")
         return DataResponse.Success(Unit)
     }
 }
